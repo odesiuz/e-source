72e8a13f36a1c516e919247a76b5b92f0c66d09a
onouviase <ono@alexi.com>
2026-01-14 22:19:30 -0500

    Add ingestion pipeline configurations and data processing scripts

---
+++ IEDR-pipeline-bundles/iedr-ingestion/src/bronze/network_circuit_data.py
@@ -0,0 +1,26 @@
+""" Created by: Ono' Uviase
+    Date: 2026-01-12
+    Time: 11:19p.m.
+    Author Email: ono@alexi.com
+    File Name: network_circuit_data.py
+"""
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+try:
+	from pyspark import pipelines as dp # type: ignore
+except ImportError:
+	raise ImportError("pyspark.pipelines module is not available in this environment.")
+
+UTILITY_ID = spark.conf.get("utility.id")
+DATA_SOURCE_PATH = f"/Volumes/workspace/network_circuits_data/{UTILITY_ID}/"
+BRONZE_TABLE = f"{UTILITY_ID}_network_delta_table"
+IEDR_CATALOG = "iedr-delta-catalog"
+
+@dp.table(name=f"`{IEDR_CATALOG}`.bronze.{BRONZE_TABLE}", table_properties={"quality": "bronze"})
+def network_circuit_data_bronze():
+	return (spark.read.format("csv").option("header", True)
+	        .option("InferSchema", True)
+	        .load(f"{DATA_SOURCE_PATH}/*.csv"))
---
+++ IEDR-pipeline-bundles/iedr-ingestion/src/bronze/planned_der_data.py
@@ -0,0 +1,25 @@
+""" Created by: Ono' Uviase
+    Date: 2026-01-14
+    Time: 8:19p.m.
+    Author Email: ono@alexi.com
+    File Name: planned_der_data.py
+"""
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+try:
+	from pyspark import pipelines as dp # type: ignore
+except ImportError:
+	raise ImportError("pyspark.pipelines module is not available in this environment.")
+
+UTILITY_ID = spark.conf.get("utility.id")
+DATA_SOURCE_PATH = f"/Volumes/workspace/planned_der_data/{UTILITY_ID}/"
+BRONZE_TABLE = f"{UTILITY_ID}_planned_der_delta_table"
+IDER_CATALOG = "iedr-delta-catalog"
+
+@dp.table(name=f"`{IDER_CATALOG}`.bronze.{BRONZE_TABLE}", table_properties={"quality": "bronze"})
+def planned_der_data_bronze():
+	return (spark.read.format("csv").option("header", True)
+	        .option("InferSchema", True)
+	        .load(f"{DATA_SOURCE_PATH}/*.csv"))
