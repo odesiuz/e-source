eb6f716724c21ee3b18e9c89fa72c75e29bd1d13
onouviase <ono@alexi.com>
2026-01-18 20:55:56 -0500

    updated schema mappings for utility_1 and utility_2

--- IEDR-pipeline-bundles/src/iedr-transformation/silver/schema_mapper.py
+++ IEDR-pipeline-bundles/src/iedr-transformation/silver/schema_mapper.py
@@ -26,23 +26,16 @@ utility_2_circuit_schema_mapping = {
 
 utility_1_installed_der_schema_mapping = {
 	"ProjectID": "utility_der_id",
-	"utility_1": "utility_id",
-	"utility_1_ProjectID": "canonical_der_id",
 	"NamePlateRating": "capacity_mw",
 	"INSTALLED": "status",
-	"CASE WHEN SolarPV > 0 → SOLAR, EnergyStorageSystem > 0 → STORAGE": "der_type",
-	"ProjectCircuitID": "utility_feeder_id",
-	"ingestion timestamp": "last_updated_ts",}
+	"ProjectCircuitID": "utility_circuit_id"}
 
 utility_2_installed_der_schema_mapping = {
 	"DER_ID": "utility_der_id",
-	"utility_2": "utility_id",
-	"utility_2_DER_ID": "canonical_der_id",
 	"DER_NAMEPLATE_RATING": "capacity_mw",
 	"INSTALLED": "status",
 	"DER_TYPE": "der_type",
-	"DER_INTERCONNECTION_LOCATION": "utility_feeder_id",
-	"ingestion timestamp": "last_updated_ts"}
+	"DER_INTERCONNECTION_LOCATION": "utility_circuit_id"}
 
 SCHEMA_REGISTRY = {
 	"utility_1": {
